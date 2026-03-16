4d60b8d8b196f503095a7dec553f7201faaa8bac
pym <646236786@qq.com>
2026-03-17 00:32:33 +0800

    .

--- source/data_processing/data_loader.py
+++ source/data_processing/data_loader.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import os
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder,OneHotEncoder,MultiLabelBinarizer
+from sklearn.preprocessing import LabelEncoder,MultiLabelBinarizer
 
 data_path=r'C:/github/project1/data/raw/'
 
@@ -36,6 +34,7 @@ data=ratings_df.merge(movies_df,on='movieid',how='left')
 data=data.merge(users_df,on='userid',how='left')
 
 #特征处理
+#用户id编码，使用
 user_encoding=LabelEncoder()
 data['userid']=user_encoding.fit_transform(data['userid'])
 n_users=len(user_encoding.classes_)
@@ -64,12 +63,34 @@ data['month']=data['timestamp'].dt.month
 
 #合并特征
 features_data=pd.concat([
-    data[['userid','movieid','sex','age','hour','day_of_week','month','rating']],
+    data[['userid','movieid','sex','age','hour','day_of_week','month','rating','timestamp']],
     job_dummis,
     types_df],
     axis=1
 )
-print(features_data.head())
-print(f'数量:{features_data.shape[1]}')
 
-features_data.to_csv(r'C:\github\project1\data\processed\processed.csv',index=False)
+train_data=[]
+val_data=[]
+test_data=[]
+
+#划分训练集，验证集，测试集，比例为0.7:0.1:0.2
+for user_id,user_val in features_data.groupby('userid'):
+    user_val.sort_values('timestamp')
+
+    n=len(user_val)
+    #把行为少于三次的直接放训练集里面
+    if n<3:
+        train_data.append(user_val)
+        continue
+    
+    train_data.append(user_val.iloc[:int(n*0.7)])
+    val_data.append(user_val.iloc[int(n*0.7):int(n*0.8)])
+    test_data.append(user_val.iloc[int(n*0.8):])
+
+train_df=pd.concat(train_data,ignore_index=True)
+val_df=pd.concat(val_data,ignore_index=True)
+test_df=pd.concat(test_data,ignore_index=True)
+
+train_df.to_csv(r'C:/github/project1/data/split/train.csv',index=False)
+val_df.to_csv(r'C:/github/project1/data/split/val.csv',index=False)
+test_df.to_csv(r'C:/github/project1/data/split/test.csv',index=False)
