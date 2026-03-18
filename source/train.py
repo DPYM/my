ea91d840c70a725d8fac0a7a912745858cf1fb01
pym <646236786@qq.com>
2026-03-19 03:43:24 +0800

    .

--- source/train.py
+++ source/train.py
@@ -13,12 +13,14 @@ train_data=pd.read_csv(r'C:/github/project1/data/splited_data/train.csv')
 #统计个数
 with open(r'C:/github/project1/data/processed_data/user_history.pkl','rb') as f:
     user_history=pickle.load(f)
-
-all_movie_ids = []
+n_movies=0
 for movie_list in user_history.values():
-    all_movie_ids.extend(movie_list)
+    if movie_list:
+        max_m=max(movie_list)
+        if max_m>n_movies:
+            n_movies=max_m
 
-n_movies=max(all_movie_ids)+1
+n_movies+=1
 n_users=len(user_history)
 
 #数据加载
@@ -28,19 +30,20 @@ data_train=User_Movie_Dataset(
 )
 #因为在dataset.py中，处理的是整个数据集而不是单个用户，导致.item()报错，所以这里把一个batch中的数据转化为tensor张量
 
-train_loader=DataLoader(data_train,batch_size=256,shuffle=True)
+train_loader=DataLoader(data_train,batch_size=256,shuffle=True,num_workers=0)
 
 model=Embedding(n_users,n_movies,dim=64)#定义模型
 model=model.to(device)
 opt=optim.Adam(model.parameters(),lr=0.0001)#采用自适应优化算法
 critersion=nn.BCELoss()#采用交叉熵作为损失函数
 
-epoch=100#循环100次
-for i in range(5):
+epoch=50#循环100次
+for i in range(epoch):
+    model.train()
     total_loss=0
     for batch in train_loader:#到这一步发现因为batch中样本长度不同报错，所以定义了一个长度阈值，见(dataset.py)
-        user_ids=batch['userid'].to(device)
-        movie_ids=batch['movieid'].to(device)
+        user_ids=batch['userid'].to(device).long()
+        movie_ids=batch['movieid'].to(device).long()
         labels=batch['label'].to(device)
 
         #计算用户和电影的向量矩阵
