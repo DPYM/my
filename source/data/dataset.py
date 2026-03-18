31f7c16cad7ec23050a8e4bf9f47c16e5e3697b3
pym <646236786@qq.com>
2026-03-18 22:33:57 +0800

    .

--- source/data/dataset.py
+++ source/data/dataset.py
@@ -4,15 +4,15 @@ from torch.utils.data import Dataset,DataLoader
 
 
 data_path=r'C:/github/project1/data/splited_data/'
-train_df=pd.read_csv(data_path+'train.csv')
-print(train_df.head())
+train_data=pd.read_csv(data_path+'train.csv')
+#print(train_df.head())
 
-'''#定义封闭接口
+#定义封闭接口
 class User_Movie_Dataset(Dataset):
-    def __init__(self,train_df):
-        self.user_ids=torch.tensor(train_df['userid'].values)
-        self.momvie_ids=torch.tensor(train_df['movieid'].values)
-        self.labels=(train_df['rating']>=4).astype(int).values
+    def __init__(self,train_data):
+        self.user_ids=torch.tensor(train_data['userid'].values)
+        self.momvie_ids=torch.tensor(train_data['movieid'].values)
+        self.labels=(train_data['rating']>=4).astype(int).values
 
     def __len__(self):
         return len(self.user_ids)
@@ -20,4 +20,4 @@ class User_Movie_Dataset(Dataset):
     def __getitem__(self,idx):
         return self.user_ids[idx],self.momvie_ids[idx],self.labels[idx]
     
-train_df=User_Movie_Dataset(train_data)'''
+train_df=User_Movie_Dataset(train_data)
